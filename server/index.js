34e0767a4219f54333c65ed6924d9fef1f4949b0
Balaganesh Mala <malabalaganesh@gmail.com>
2026-03-28 23:27:00 +0530

    port

--- server/index.js
+++ server/index.js
@@ -28,6 +28,7 @@ app.use(cors({
         'http://127.0.0.1:5173', 
         'http://127.0.0.1:5174',
         'http://localhost:5000',
+        'http://187.127.138.157:5000/',
         'https://finwise-64g1.onrender.com',
         'https://finwise-student.vercel.app', // Student Portal Deployed
         process.env.CLIENT_URL,
